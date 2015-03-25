5049d79aa17999f8f82ae6764215603597d766b9
HDking <h.c.dinh1@gmail.com>
2015-03-25 22:34:24 +0100

    Chapter 4

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,17 +1,21 @@
 require 'spec_helper'
 
-describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_index_path
-      response.status.should be(200)
+describe "Static pages"  do
+  describe "Home page" do
+    it "should have the h1 'Sample App' " do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Sample App')
     end
-  end
-end
 
-describe "Home page" do
-	it "should have the content 'Sample App'" do
-		visit '/static pages/home'
-			page.should have content('Sample App')
+    it "should have the base title" do
+      visit '/static-pages/home'
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
+
+  end
 end
